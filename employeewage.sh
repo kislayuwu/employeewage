f2b80c1da292468a20de8d7e88a35de6c5e0fa40
Kislay Shekhar <kislayshekhar2@gmail.com>
2024-01-14 03:33:58 +0530

    monthly salary

--- employeewage.sh
+++ employeewage.sh
@@ -1,23 +1,3 @@
-isPresent=1
-checkAttendence=$((RANDOM%2))
-if [ $isPresent -eq $checkAttendence ]
-then
-        echo "Employee is present"
-else
-        echo "absent"
-
-fi
-
-hoursPerDay=8
-wagePerHour=20
-dailyWage=$((hoursPerDay*wagePerHour))
-echo "daily employee wage is $dailyWage"
-
-partTimeHours=8
-wagePerHour=20
-partTimeWage=$((partTimeHours*wagePerHour))
-echo "Part time wage is $partTimeWage"
-
 days=20
 monthlySalary=$((days*dailyWage))
 echo "monthly salary is $monthlySalary"

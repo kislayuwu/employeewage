30a5c6a45f860c14ea63fc687b6bc054936bffce
Kislay Shekhar <kislayshekhar2@gmail.com>
2024-01-15 18:05:00 +0530

    switch case added

--- employeewage.sh
+++ employeewage.sh
@@ -18,6 +18,14 @@ wagePerHour=20
 partTimeWage=$((partTimeHours*wagePerHour))
 echo "Part time wage is $partTimeWage"
 
+wage="dailywage"
+case "$wage" in
+	"dailywage") echo "daily wage is $dailyWage"
+;;
+	"parttimewage") echo "part time wage is $partTimeWage"
+;;
+esac
+
 days=20
 monthlySalary=$((days*dailyWage))
 echo "monthly salary is $monthlySalary"
